43f1c1b912e925aad6d90f44a156515f157c2f8e
Kosenkov <9588434@gmail.com>
2022-08-17 13:48:00 +0300

    Add Example 005 Base

---
+++ Example005_Base/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write ("Insert first numbers: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Insert second numbers: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Insert third numbers: ");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Insert fourth numbers: ");
+int d = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Insert fifth numbers: ");
+int e = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write ("Maximum number = ");
+Console.WriteLine (max);
