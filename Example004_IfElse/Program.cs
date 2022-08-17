4af5c94ea52c726a5113106b55bba4a9ef58bff1
Kosenkov <9588434@gmail.com>
2022-08-17 12:57:11 +0300

    Add Example 004 if and else

---
+++ Example004_IfElse/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write ("Insert user name: ");
+String username = Console.ReadLine ();
+if (username.ToLower() == "ivan") {
+Console.WriteLine ("Hello meeeeeennnnnnn!!!!!");
+}
+else {
+Console.WriteLine ("Fuck you meeeeeeennnnnnn!!!!!");
+}
