78ac4d876c8f4ce54c2f49e0384066c216d7f1c1
Kosenkov <9588434@gmail.com>
2022-08-17 12:44:51 +0300

    Add Example 003 Sum a + b

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = new Random().Next(1,10);
+int b = new Random().Next(1,10);
+Console.WriteLine (a);
+Console.WriteLine (b);
+int sum = a + b;
+Console.Write ("Sum = ");
+Console.Write (sum);
