5746d5a9e730d3deb12ea3a644e5c606c1e5ec98
Kosenkov <9588434@gmail.com>
2022-08-18 12:38:38 +0300

    Add distance and time to Example 007 Dog

--- Example007_Dog/Program.cs
+++ Example007_Dog/Program.cs
@@ -1,29 +1,37 @@
 ﻿int count = 0;
+int friend = 2;
+double time = 0;
+double all_time = 0;
+double all_dist = 0;
+
 Console.Write("Введите расстояние в метрах: ");
 double dist = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите скорость 1 человека км/ч: ");
-int speed_1 = Convert.ToInt32(Console.ReadLine());
+double speed_1 = Math.Round(Convert.ToDouble(Console.ReadLine()) * 1000 / 3600, 2);
 Console.Write("Введите скорость 2 человека км/ч: ");
-int speed_2 = Convert.ToInt32(Console.ReadLine());
+double speed_2 = Math.Round(Convert.ToDouble(Console.ReadLine()) * 1000 / 3600, 2);
 Console.Write("Введите скорость собаки км/ч: ");
-int speed_dog = Convert.ToInt32(Console.ReadLine());
-int friend = 2;
-double time = 0;
-while (dist > 5)
+double speed_dog = Math.Round(Convert.ToDouble(Console.ReadLine()) * 1000 / 3600, 2);
+
+while (dist > 1)
 {
     if (friend == 1)
     {
         time = dist / (speed_1 + speed_dog);
         friend = 2;
+        all_time = all_time + time;
     }
     else
     {
         time = dist / (speed_2 + speed_dog);
         friend = 1;
+        all_time = all_time + time;
     }
     dist = dist - (speed_1 + speed_2) * time;
+    all_dist = all_dist + dist;
     count++;
 }
-Console.Write ("Собака пробежала: ");
-Console.Write (count);
-Console.Write (" раз");
+Console.Write($"Собака пробежала {Math.Round(all_dist, 2)} м. ");
+Console.Write($"и развернулась {count} раз за ");
+var ts = TimeSpan.FromSeconds(Math.Round(all_time, 0));
+Console.WriteLine("{1} ч. {2} м. {3} с.", ts.Days, ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
