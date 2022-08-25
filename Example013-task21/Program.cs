914383b58ee5469fbde0a471cd6aa455b580d2d7
Kosenkov <9588434@gmail.com>
2022-08-25 21:40:11 +0300

    Add Task 21

---
+++ Example013-task21/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите координаты точки А: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки В: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double GetDistance(int a, int b, int c, int d)
+{
+double result = Math.Sqrt((c - a)*(c - a) + (d - b)*(d - b));
+return result; 
+}
+
+double res = GetDistance (x1, y1, x2, y2);
+Console.WriteLine(res);
