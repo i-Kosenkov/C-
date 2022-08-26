6d73f3203d400a6a6ee0f4771692f34fcb3142c2
Kosenkov <9588434@gmail.com>
2022-08-26 09:51:46 +0300

    Add task 22, done. Include PrintArray()

---
+++ Example014-task22/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Введите число N: ");
+bool isNum_x = int.TryParse(Console.ReadLine(), out int x);
+
+if (!isNum_x || x <= 0)
+{
+    Console.WriteLine("Ошибка ввода");
+    return;
+}
+
+int[] GetSqrt(int z)
+{
+    int i = 1;
+    int[] array = new int[z];
+    while (i <= z)
+    {
+        array[i - 1] = (int)Math.Pow(i, 2);
+        i++;
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int i = 0;
+    int lenght = array.Length;
+    while (i < lenght)
+    {
+        Console.Write($"{array[i]} ");
+        i++;
+    }
+}
+
+PrintArray(GetSqrt(x));
