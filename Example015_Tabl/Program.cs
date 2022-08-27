f8f5275547bf9717f49054373778800befa569bb
Kosenkov <9588434@gmail.com>
2022-08-27 13:01:03 +0300

    Add Example 15 Table + Change Char in the text

---
+++ Example015_Tabl/Program.cs
@@ -0,0 +1,26 @@
+﻿//Табилца умножения
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} * {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+
+string text = "Как нам повторить"
+            + "То, что ушло навсегда?"
+            + "Знает лишь вечность...";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+Console.Write(Replace(text, " ", "|"));
