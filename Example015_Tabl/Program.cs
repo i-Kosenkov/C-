3ab02074cc3c3717c8ce8fb1508981357b5982cd
Kosenkov <9588434@gmail.com>
2022-08-28 10:44:14 +0300

    Add Example 15 Tabl, Change Char, Massive sort

--- Example015_Tabl/Program.cs
+++ Example015_Tabl/Program.cs
@@ -8,19 +8,64 @@
 //     Console.WriteLine();
 // }
 
-string text = "Как нам повторить"
-            + "То, что ушло навсегда?"
-            + "Знает лишь вечность...";
+//Замена в тексте символа
+// string text = "Как нам повторить"
+//             + "То, что ушло навсегда?"
+//             + "Знает лишь вечность...";
 
-string Replace(string text, char oldValue, char newValue)
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = string.Empty;
+//     for (int i = 0; i < text.Length; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+//     return result;
+// }
+
+// Console.Write(Replace(text, ' ', '|'));
+
+
+//Сортировка массива
+int[] arr = { 7, 5, 3, 1, 2, 8, 4, 6, 1, 0, 3 };
+
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] > array[minPosition]) minPosition = j;
+//         }
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+// }
+
+//Перевернуть массив
+void ReverseSort(int[] array)
 {
-    string result = string.Empty;
-    for (int i = 0; i < text.Length; i++)
+    for (int i = 0; i < array.Length / 2; i++)
     {
-        if (text[i] == oldValue) result = result + $"{newValue}";
-        else result = result + $"{text[i]}";
+        int temporary = array[array.Length - i - 1];
+        array[array.Length - i - 1] = array[i];
+        array[i] = temporary;
     }
-    return result;
 }
 
-Console.Write(Replace(text, " ", "|"));
+//Вывод массива на экран
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(arr);
+ReverseSort(arr);
+PrintArray(arr);
