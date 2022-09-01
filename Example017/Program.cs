0262396cc3292ce5b5c4cb64bf549e7d9c951ace
Kosenkov <9588434@gmail.com>
2022-09-01 12:21:17 +0300

    Example 17

---
+++ Example017/Program.cs
@@ -0,0 +1,32 @@
+﻿
+//Вам дана строка состоящая только из цифр. Вам нужно посчитать сколько нулей ("0") находится в начале строки. 
+//Входные данные: Строка (0-9) 
+
+Console.Write("Введите число: ");
+string x = Console.ReadLine();
+
+if (x[0] != '0')
+{
+    Console.WriteLine("Первая цифра не 0");
+    return;
+}
+
+int GetZero(string a)
+{
+    int lenght = a.Length;
+    int result = 0;
+    for (int i = 0; i < lenght; i++)
+    {
+        if (a[i] == '0')
+        {
+            result++;
+        }
+        else
+        {
+            return result;
+        }
+    }
+    return result;
+}
+
+Console.WriteLine(GetZero(x));
---
+++ Example018/Program.cs
@@ -0,0 +1,29 @@
+﻿//Разделите строку на массив из пар по два символа. Если строка содержит нечетное количество символов, 
+//пропущенный второй символ последней пары должен быть заменен подчеркиванием "_". 
+//Входные данные: Строка (0 <= len(str) <= 100 ). 
+
+// "abcd" == ["ab", "cd"]
+// "abc" == ["ab", "c_"]
+// "abcdf" == ["ab", "cd", "f_"]
+// "a" == ["a_"]
+// "" == [""]
+
+Console.Write("Введите значение: ");
+string x = Console.ReadLine();
+
+int l = x.Length;
+if (l % 2 != 0) x = x + '_';
+
+string[] GetNumber(string a)
+{
+    int y = 0;
+    string[] array = new string[a.Length / 2];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = a[y] + "" + a[y + 1];
+        y = y + 2;
+    }
+    return array;
+}
+
+Console.WriteLine(string.Join(" ", GetNumber(x)));
