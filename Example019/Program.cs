4e2d9ce8d83f2fa6020ca445659aae7567dc846c
Kosenkov <9588434@gmail.com>
2022-09-01 21:25:36 +0300

    Task 32

--- Example019/Program.cs
+++ Example019/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Проверка вводимого числа 
+Console.Write("Введите длину массива: ");
+bool isNum_x = int.TryParse(Console.ReadLine(), out int x);
+
+if (!isNum_x)
+{
+    Console.WriteLine("Ошибка ввода");
+    return;
+}
+
+int [] array = GetArray(x);
+
+Console.Write("Введите искомое значение: ");
+bool isNum_y = int.TryParse(Console.ReadLine(), out int y);
+
+Console.WriteLine(GetNumberTrueFalse(array,y));
+
+bool GetNumberTrueFalse(int[] array, int a)
+{
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == a) return true;
+}
+    return false;
+}
+
+//Вывод данных из массива
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+//Заполняем массив
+int[] GetArray(int lenght)
+{
+    int[] array = new int[lenght];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i} элемент массива:");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
