6f5ca2f2519dacf237f4662525ee41166c31c175
Kosenkov <9588434@gmail.com>
2022-09-09 21:29:40 +0300

    Task 51 variant 1 and 2

--- Example031-task51/Program.cs
+++ Example031-task51/Program.cs
@@ -23,26 +23,42 @@ void PrintArray(int[,] array)
     }
 }
 
-//Находим сумму элементов диагонали
+//Находим сумму элементов диагонали версия 1
 int GetSumDiagonalNumbers(int[,] array)
 {
     int result = 0;
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = i; j < array.GetLength(1); j++)
         {
             if (i == j)
             {
                 result = result + array[i, j];
+                break;
             }
         }
     }
     return result;
 }
 
-int[,] array = new int[5, 5];
+//Находим сумму элементов диагонали версия 2
+int GetSumDiagonalNumbers2(int[,] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if (i >= array.GetLength(1))
+        {
+            break;
+        }
+        result = result + array[i, i];
+    }
+    return result;
+}
+
+int[,] array = new int[5, 3];
 GetRandomDoubleArray(array);
 PrintArray(array);
-int sum = GetSumDiagonalNumbers(array);
+int sum = GetSumDiagonalNumbers2(array);
 Console.WriteLine();
 Console.Write($"Сумма диагонали = {sum}");
